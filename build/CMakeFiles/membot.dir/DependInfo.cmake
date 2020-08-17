0f7c0f733434ff3be42da9631f8ab2d03cf87bdb
Sourabh Misal <sourabhmisal1@gmail.com>
2020-08-17 20:38:36 +0530

    first commit, Task 3

---
+++ build/CMakeFiles/membot.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sourabh/cpp/cpp/CppND-Memory-Management-Chatbot-master/src/chatbot.cpp" "/home/sourabh/cpp/cpp/CppND-Memory-Management-Chatbot-master/build/CMakeFiles/membot.dir/src/chatbot.cpp.o"
+  "/home/sourabh/cpp/cpp/CppND-Memory-Management-Chatbot-master/src/chatgui.cpp" "/home/sourabh/cpp/cpp/CppND-Memory-Management-Chatbot-master/build/CMakeFiles/membot.dir/src/chatgui.cpp.o"
+  "/home/sourabh/cpp/cpp/CppND-Memory-Management-Chatbot-master/src/chatlogic.cpp" "/home/sourabh/cpp/cpp/CppND-Memory-Management-Chatbot-master/build/CMakeFiles/membot.dir/src/chatlogic.cpp.o"
+  "/home/sourabh/cpp/cpp/CppND-Memory-Management-Chatbot-master/src/graphedge.cpp" "/home/sourabh/cpp/cpp/CppND-Memory-Management-Chatbot-master/build/CMakeFiles/membot.dir/src/graphedge.cpp.o"
+  "/home/sourabh/cpp/cpp/CppND-Memory-Management-Chatbot-master/src/graphnode.cpp" "/home/sourabh/cpp/cpp/CppND-Memory-Management-Chatbot-master/build/CMakeFiles/membot.dir/src/graphnode.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_FILE_OFFSET_BITS=64"
+  "__WXGTK__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/lib/wx/include/gtk2-unicode-static-3.0"
+  "/usr/local/include/wx-3.0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
